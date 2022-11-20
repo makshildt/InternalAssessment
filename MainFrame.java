5a165b1a5a52dc3e96bcc18d4b725c34e19c5616
Maksymilian Hildt <113469065+makshildt@users.noreply.github.com>
2022-11-20 23:02:36 +0100

    Add files via upload

--- MainFrame.java
+++ MainFrame.java
@@ -9,12 +9,22 @@ import javax.swing.JButton;
 import java.awt.Color;
 import java.awt.CardLayout;
 import java.awt.event.ActionListener;
+import java.sql.ResultSet;
+import java.util.Arrays;
 import java.awt.event.ActionEvent;
 import javax.swing.SwingConstants;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.JComboBox;
 import java.awt.GridLayout;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import java.awt.Insets;
+
+//Primary key means has to be unique, this means it can be used in other tables aswell
+//foreign keys unique attributed, used to link 2 tables together, 
 
 public class MainFrame extends JFrame {
 
@@ -33,15 +43,40 @@ public class MainFrame extends JFrame {
 	Font normaltext = new Font("Monospaced", Font.PLAIN, 20);
 	Font title = new Font("Monospaced", Font.PLAIN, 30);
 	public static String team = "Team: "; 
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
+	private JTextField textField_4;
+	private JTextField textField_5;
 
 	/**
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
+		//TestDb db = new TestDb();
+	
+
+		//EXAMPLE CODE FOR QUEREY
+//		try {
+//			db.connect();
+//			ResultSet resultSet = db.query("SELECT first_name FROM USERS;");
+//
+//			while (resultSet.next()) {
+//				String name = resultSet.getString(1);
+//				System.out.println(name);
+//			}
+//
+//			db.disconnect();
+//		} catch (Exception e) {
+//			System.out.println(e.getMessage());
+//		}
+//
+//		
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					MainFrame frame = new MainFrame();
+				MainFrame frame = new MainFrame();
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -166,10 +201,168 @@ public class MainFrame extends JFrame {
 
 
 		//PANEL 3 ----------------------
-		
+
 		JPanel panel3 = new JPanel();
 		panel3.setBackground(Color.YELLOW);
 		PanelMain.add(panel3, "name_53919315779100");
+		GridBagLayout gbl_panel3 = new GridBagLayout();
+		gbl_panel3.columnWidths = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0};
+		gbl_panel3.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+		gbl_panel3.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, Double.MIN_VALUE};
+		gbl_panel3.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		panel3.setLayout(gbl_panel3);
+		
+		JLabel lblNewLabel = new JLabel("Enter New Employees");
+		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
+		gbc_lblNewLabel.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel.gridx = 5;
+		gbc_lblNewLabel.gridy = 1;
+		panel3.add(lblNewLabel, gbc_lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("First Name:");
+		GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
+		gbc_lblNewLabel_1.anchor = GridBagConstraints.EAST;
+		gbc_lblNewLabel_1.fill = GridBagConstraints.VERTICAL;
+		gbc_lblNewLabel_1.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_1.gridx = 4;
+		gbc_lblNewLabel_1.gridy = 4;
+		panel3.add(lblNewLabel_1, gbc_lblNewLabel_1);
+		
+		textField = new JTextField();
+		GridBagConstraints gbc_textField = new GridBagConstraints();
+		gbc_textField.insets = new Insets(0, 0, 5, 5);
+		gbc_textField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField.gridx = 5;
+		gbc_textField.gridy = 4;
+		panel3.add(textField, gbc_textField);
+		textField.setColumns(10);
+		
+		JLabel lblNewLabel_2 = new JLabel("Last Name:");
+		GridBagConstraints gbc_lblNewLabel_2 = new GridBagConstraints();
+		gbc_lblNewLabel_2.anchor = GridBagConstraints.EAST;
+		gbc_lblNewLabel_2.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_2.gridx = 4;
+		gbc_lblNewLabel_2.gridy = 5;
+		panel3.add(lblNewLabel_2, gbc_lblNewLabel_2);
+		
+		textField_1 = new JTextField();
+		GridBagConstraints gbc_textField_1 = new GridBagConstraints();
+		gbc_textField_1.insets = new Insets(0, 0, 5, 5);
+		gbc_textField_1.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField_1.gridx = 5;
+		gbc_textField_1.gridy = 5;
+		panel3.add(textField_1, gbc_textField_1);
+		textField_1.setColumns(10);
+		
+		JLabel lblNewLabel_3 = new JLabel("Year of Birth: ");
+		GridBagConstraints gbc_lblNewLabel_3 = new GridBagConstraints();
+		gbc_lblNewLabel_3.anchor = GridBagConstraints.EAST;
+		gbc_lblNewLabel_3.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_3.gridx = 4;
+		gbc_lblNewLabel_3.gridy = 6;
+		panel3.add(lblNewLabel_3, gbc_lblNewLabel_3);
+		
+		textField_2 = new JTextField();
+		GridBagConstraints gbc_textField_2 = new GridBagConstraints();
+		gbc_textField_2.insets = new Insets(0, 0, 5, 5);
+		gbc_textField_2.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField_2.gridx = 5;
+		gbc_textField_2.gridy = 6;
+		panel3.add(textField_2, gbc_textField_2);
+		textField_2.setColumns(10);
+		
+		JLabel lblNewLabel_4 = new JLabel("Email: ");
+		GridBagConstraints gbc_lblNewLabel_4 = new GridBagConstraints();
+		gbc_lblNewLabel_4.anchor = GridBagConstraints.EAST;
+		gbc_lblNewLabel_4.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_4.gridx = 4;
+		gbc_lblNewLabel_4.gridy = 7;
+		panel3.add(lblNewLabel_4, gbc_lblNewLabel_4);
+		
+		textField_3 = new JTextField();
+		GridBagConstraints gbc_textField_3 = new GridBagConstraints();
+		gbc_textField_3.insets = new Insets(0, 0, 5, 5);
+		gbc_textField_3.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField_3.gridx = 5;
+		gbc_textField_3.gridy = 7;
+		panel3.add(textField_3, gbc_textField_3);
+		textField_3.setColumns(10);
+		
+		JLabel lblNewLabel_5 = new JLabel("Role: ");
+		GridBagConstraints gbc_lblNewLabel_5 = new GridBagConstraints();
+		gbc_lblNewLabel_5.anchor = GridBagConstraints.EAST;
+		gbc_lblNewLabel_5.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_5.gridx = 4;
+		gbc_lblNewLabel_5.gridy = 8;
+		panel3.add(lblNewLabel_5, gbc_lblNewLabel_5);
+		
+		textField_4 = new JTextField();
+		GridBagConstraints gbc_textField_4 = new GridBagConstraints();
+		gbc_textField_4.insets = new Insets(0, 0, 5, 5);
+		gbc_textField_4.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField_4.gridx = 5;
+		gbc_textField_4.gridy = 8;
+		panel3.add(textField_4, gbc_textField_4);
+		textField_4.setColumns(10);
+		
+		JLabel lblNewLabel_6 = new JLabel("ID: ");
+		GridBagConstraints gbc_lblNewLabel_6 = new GridBagConstraints();
+		gbc_lblNewLabel_6.anchor = GridBagConstraints.EAST;
+		gbc_lblNewLabel_6.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_6.gridx = 4;
+		gbc_lblNewLabel_6.gridy = 9;
+		panel3.add(lblNewLabel_6, gbc_lblNewLabel_6);
+		
+		textField_5 = new JTextField();
+		GridBagConstraints gbc_textField_5 = new GridBagConstraints();
+		gbc_textField_5.insets = new Insets(0, 0, 5, 5);
+		gbc_textField_5.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField_5.gridx = 5;
+		gbc_textField_5.gridy = 9;
+		panel3.add(textField_5, gbc_textField_5);
+		textField_5.setColumns(10);
+		
+		JLabel lblNewLabel_7 = new JLabel("Team: ");
+		GridBagConstraints gbc_lblNewLabel_7 = new GridBagConstraints();
+		gbc_lblNewLabel_7.anchor = GridBagConstraints.EAST;
+		gbc_lblNewLabel_7.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_7.gridx = 4;
+		gbc_lblNewLabel_7.gridy = 10;
+		panel3.add(lblNewLabel_7, gbc_lblNewLabel_7);
+		
+		JComboBox comboBox = new JComboBox();
+		GridBagConstraints gbc_comboBox = new GridBagConstraints();
+		gbc_comboBox.insets = new Insets(0, 0, 5, 5);
+		gbc_comboBox.fill = GridBagConstraints.HORIZONTAL;
+		gbc_comboBox.gridx = 5;
+		gbc_comboBox.gridy = 10;
+		panel3.add(comboBox, gbc_comboBox);
+		GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
+		gbc_btnNewButton.insets = new Insets(0, 0, 0, 5);
+		gbc_btnNewButton.gridx = 5;
+		gbc_btnNewButton.gridy = 11;
+
+        String[] info = new String[6];
+        String name;
+		
+		JButton btnNewButton = new JButton("Submit");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+                //when submit button is clicked, the information is sent to the string array
+                String name = textField.getText();
+                info[1] = textField_1.getText();
+                info[2] = textField_2.getText();
+                info[3] = textField_3.getText();
+                info[4] = textField_4.getText();
+                info[5] = textField_5.getText();
+			}
+		});
+		
+        //print array info using tostring
+        //System.out.println(Arrays.toString(info));
+        System.out.println(name);
+
+		panel3.add(btnNewButton, gbc_btnNewButton);
 		
 		JPanel panel4 = new JPanel();
 		panel4.setBackground(Color.GREEN);
@@ -255,4 +448,4 @@ public class MainFrame extends JFrame {
 		
 		
 	}
-}
+};
